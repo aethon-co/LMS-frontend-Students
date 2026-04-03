eb10cc7a2c126f0baa8dab36ef4add6a736c4d60
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2026-04-03 12:55:06 +0530

    url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/student',
+  baseURL: import.meta.env.VITE_API_URL || 'https://lms-backend-ivory-one.vercel.app/api/student',
 });
 
 api.interceptors.request.use((config) => {
