cc63532731a1a2bae814151436f97ce36799f6b1
Anantha Krishnan <jananthak@gmail.com>
2026-04-05 09:30:02 +0530

    fixes

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://lms-backend-ivory-one.vercel.app/api/student',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
